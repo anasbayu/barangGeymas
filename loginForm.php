7d62a64e777b6be62a5efa31606c5e5e8c46c27b
Mukhamad Mukhsin Wibowo <mmwibowo2@gmail.com>
2016-01-05 09:58:00 +0700

    Front end

--- loginForm.php
+++ loginForm.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Masuk Aplikasi</title>
+        <title>Formulir Masuk</title>
         <link type="text/css" rel="stylesheet" href="css/normalize.css"/>
         <link type="text/css" rel="stylesheet" href="css/grid.css"/>
         <link type="text/css" rel="stylesheet" href="css/style.css"/>
@@ -13,15 +13,15 @@
 
             <div class="grid-4">
                 <div class="form-login">
-                    <div class="form-title">Masuk ke Aplikasi</div>
+                    <div class="form-title">Formulir Masuk</div>
 
                     <div class="form-body">
                         <form method="post" action="logic/login.php">
                             <!-- <label for="username">Username</label> -->
-                            <input class="form-input" type="text" maxlength="25" placeholder="username" name="username" required autofocus/><br>
+                            <input class="form-input" type="text" maxlength="25" placeholder="Username" name="username" required autofocus/>
 
                             <!-- <label for="password">Password</label> -->
-                            <input class="form-input" type="password" maxlength="25" placeholder="password" name="password" required/><br>
+                            <input class="form-input" type="password" maxlength="25" placeholder="Password" name="password" required/>
 
                             <button type="submit">Masuk</button>
                         </form>
--- registerForm.php
+++ registerForm.php
@@ -3,33 +3,94 @@
    include "include/koneksi.php";
 ?>
 <html>
-   <head>
-      <title>Register</title>
-   </head>
-   <body>
-      <form method="post" action="logic/registrasi.php">
-         <label for="username">Username :</label>
-         <input type="text" maxlength="25" placeholder="username" name="username" required/><br>
-         <label for="password">Password :</label>
-         <input type="password" maxlength="25" placeholder="password" name="password" required/><br>
-         <label for="konfirmasi">Konfirmasi :</label>
-         <input type="password" maxlength="25" placeholder="Konfirmasi password" name="konfirmasi" required/><br>
-         <label for="kelamin">Kelamin :</label>
-         <?php
+    <head>
+        <title>Formulir Pendaftaran</title>
+        <link type="text/css" rel="stylesheet" href="css/normalize.css"/>
+        <link type="text/css" rel="stylesheet" href="css/grid.css"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <link rel="shortcut icon" href="img/icon.svg"/>
+    </head>
+    <body>
+        <div class="grid-container">
+            <div class="grid-4"></div>
+
+            <div class="grid-4">
+                <div class="form-signup">
+                    <div class="form-title">Formulir Pendaftaran</div>
+
+                    <div class="form-body">
+                        <form method="post" action="logic/registrasi.php">
+                            <label for="username">Username</label>
+                            <input class="form-input" type="text" maxlength="25" placeholder="Username" name="username" required autofocus/>
+
+                            <label for="password">Password</label>
+                            <input class="form-input" type="password" maxlength="25" placeholder="Password" name="password" required/>
+
+                            <label for="konfirmasi">Konfirmasi</label>
+                            <input class="form-input" type="password" maxlength="25" placeholder="Password" name="konfirmasi" required/><br>
+
+                            <label for="kelamin">Jenis Kelamin</label>
+
+                            <?php
+                            $query = "SELECT * FROM kelamin ORDER BY id_kelamin";
+                            ?>
+
+                            <select class="form-input" name="kelamin">
+
+                            <?php
+                                foreach($conn->query($query) as $kelamin) {
+                                    echo "<option value='$kelamin[0]'>$kelamin[1]</option>";
+                                }
+                            ?>
+
+                            </select>
+
+                            <label for="alamat">Alamat</label>
+                            <textarea class="form-input" name="alamat" placeholder="Alamat"></textarea>
+
+                            <button type="submit">Daftar</button>
+                        </form>
+
+                        <span>Sudah punya akun? <a href="loginForm.php">Masuk sekarang!</a></span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="grid-4"></div>
+        </div>
+
+        <!-- <form method="post" action="logic/registrasi.php">
+            <label for="username">Username :</label>
+            <input type="text" maxlength="25" placeholder="username" name="username" required/><br>
+
+            <label for="password">Password :</label>
+            <input type="password" maxlength="25" placeholder="password" name="password" required/><br>
+
+            <label for="konfirmasi">Konfirmasi :</label>
+            <input type="password" maxlength="25" placeholder="Konfirmasi password" name="konfirmasi" required/><br>
+
+            <label for="kelamin">Kelamin :</label>
+
+            <?php
             $query = "SELECT * FROM kelamin ORDER BY id_kelamin";
-         ?>
-         <select name="kelamin">
-         <?php
-            foreach($conn->query($query) as $kelamin)
-            {
-               echo "<option value='$kelamin[0]'>$kelamin[1]</option>";
-            }
-         ?>
-         </select><br>
-         <label for="alamat">Alamat :</label>
-         <textarea name="alamat" placeholder="alamat"></textarea><br>
-         <button>Daftar</button>
-      </form>
-      <span>Sudah punya akun? <b><a href="loginForm.php">masuk sekarang!</a></n></span>
-   </body>
+            ?>
+
+            <select name="kelamin">
+
+            <?php
+                foreach($conn->query($query) as $kelamin) {
+                    echo "<option value='$kelamin[0]'>$kelamin[1]</option>";
+                }
+            ?>
+
+            </select><br>
+
+            <label for="alamat">Alamat :</label>
+            <textarea name="alamat" placeholder="alamat"></textarea><br>
+
+            <button>Daftar</button>
+        </form>
+
+        <span>Sudah punya akun? <b><a href="loginForm.php">masuk sekarang!</a></n></span> -->
+    </body>
 </html>
